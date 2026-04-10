59c66463a6d90b48c027db0686b224e12a2f4f05
Korarei <132204522+korarei@users.noreply.github.com>
2026-04-11 04:56:55 +0900

    Refactor: Auto-generate .gitignore when creating venv

--- astra/core/venv.py
+++ astra/core/venv.py
@@ -14,23 +14,37 @@ logger = getLogger(__name__)
 def venv(dst: Path, version: str | None) -> None:
     logger.info("Setting up virtual environment to: %s", dst)
 
-    dst /= ".venv/aviutl2"
+    dst /= ".venv"
     shutil.rmtree(dst, ignore_errors=True)
-    dst.mkdir(parents=True, exist_ok=True)
-    dst = dst.resolve()
+
+    aviutl2 = dst / "aviutl2"
+    aviutl2.mkdir(parents=True, exist_ok=True)
+    aviutl2 = aviutl2.resolve()
 
     if version is None:
         url = "https://raw.githubusercontent.com/Neosku/aviutl2-catalog-data/main/index.json"
         identifier = "Kenkun.AviUtlExEdit2"
-        with cast(BinaryIO, urlopen(url)) as response:
-            content = response.read().decode("utf-8")
-            data = cast(list[dict[str, str]], json.loads(content))
-            if item := next((i for i in data if i["id"] == identifier), None):
-                version = item["latest-version"]
+
+        try:
+            with cast(BinaryIO, urlopen(url)) as response:
+                content = response.read().decode("utf-8")
+        except Exception:
+            shutil.rmtree(dst, ignore_errors=True)
+            raise
+
+        data = cast(list[dict[str, str]], json.loads(content))
+        if item := next((i for i in data if i["id"] == identifier), None):
+            version = item["latest-version"]
 
     url = f"https://spring-fragrance.mints.ne.jp/aviutl/aviutl2{version}.zip"
-    download(url, dst)
 
-    (dst / "data").mkdir(parents=True, exist_ok=True)
+    try:
+        download(url, aviutl2)
+    except Exception:
+        shutil.rmtree(dst, ignore_errors=True)
+        raise
+
+    (aviutl2 / "data").mkdir(parents=True, exist_ok=True)
+    _ = (dst / ".gitignore").write_text("*", encoding="utf-8", newline="\n")
 
     logger.info("Setting up virtual environment completed")
